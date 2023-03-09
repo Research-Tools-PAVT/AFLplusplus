1113a93feb657d51c8e58f04928745cbf9b30868
Sujit Muduli <smuduli@iitk.ac.in>
2023-03-09 13:35:09 +0530

    updated test-constraints

--- test/test-constraints.c
+++ test/test-constraints.c
@@ -9,9 +9,9 @@
 
 #ifdef FUZZMAX
   #include "fuzzmax.h"
-#endif
+  #define SHM_FUZZMAX_ENV_VAR "__AFL_SHM_FUZZMAX_ID"
 
-#define SHM_FUZZMAX_ENV_VAR "__AFL_SHM_FUZZMAX_ID"
+#endif
 
 __AFL_FUZZ_INIT();
 
@@ -28,6 +28,9 @@ int main(void) {
   }
 #endif
 
+  uint32_t npreds = 3;
+  uint8_t  T[3] = {0};
+
   __AFL_INIT();
   int *magic = (int *)__AFL_FUZZ_TESTCASE_BUF;
 
@@ -39,18 +42,36 @@ int main(void) {
     int y = magic[1];
     int z = magic[2];
 
-    uint8_t counter = 0;
-    if (x > y) ++counter;
-    if (x + y == z) ++counter;
-    if (z > 5 * y) ++counter;
+    T[0] = (x > y);       // ++counter;
+    T[1] = (x + y == z);  // ++counter;
+    T[2] = (z > 5 * y);   //  ++counter;
+
+#ifdef FUZZMAX
+    __afl_fuzzmax_ptr->fuzzmax_counter = 0;
+    for (u32 rid = 0; rid < npreds; ++rid) {
+      if (T[rid] == 0) continue;
+      __afl_fuzzmax_ptr->fuzzmax_counter += T[rid];
+      __afl_fuzzmax_ptr->histogram[rid] = T[rid];
+
+      for (u32 cid = 0; cid <= rid; ++cid)
+        __afl_fuzzmax_ptr->bpp_predicates_map[rid * npreds + cid] =
+            T[rid] && T[cid];
+    }
+
+    if (__afl_fuzzmax_ptr->fuzzmax_counter == npreds) assert(0);
+
+#else
+
+    uint32_t counter = 0;
+    for (uint32_t rid = 0; rid < npreds; ++rid) {
+      counter += T[rid];
+    }
 
     if (counter == 3) {
       printf("x = %d, y = %d, z = %d\n", x, y, z);
       assert(0);
     }
 
-#ifdef FUZZMAX
-    __afl_fuzzmax_ptr->fuzzmax_counter = counter;
 #endif
   }
 
