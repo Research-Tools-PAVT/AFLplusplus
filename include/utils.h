9a8de3a03deaccd5cb6ff8930d84e9a0d934e8f2
Sujit Muduli <smuduli@iitk.ac.in>
2023-08-12 14:39:59 +0530

    wip: save_if_interesting to recognize num preds

--- include/utils.h
+++ include/utils.h
@@ -1,27 +1,30 @@
 /*
-    american fuzzy lop++ - afl-proxy skeleton example
-    ---------------------------------------------------
+   american fuzzy lop++ - afl-proxy skeleton example
+   ---------------------------------------------------
 
-    Written by Marc Heuse <mh@mh-sec.de>
+   Written by Marc Heuse <mh@mh-sec.de>
 
-    Copyright 2019-2023 AFLplusplus Project. All rights reserved.
+   Copyright 2019-2023 AFLplusplus Project. All rights reserved.
 
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at:
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at:
 
-    http://www.apache.org/licenses/LICENSE-2.0
-
-
-    HOW-TO
-    ======
-
-    You only need to change the while() loop of the main() to send the
-    data of buf[] with length len to the target and write the coverage
-    information to __afl_area_ptr[__afl_map_size]
+   http://www.apache.org/licenses/LICENSE-2.0
 
 
- */
+   HOW-TO
+   ======
+
+   You only need to change the while() loop of the main() to send the
+   data of buf[] with length len to the target and write the coverage
+   information to __afl_area_ptr[__afl_map_size]
+
+
+*/
+
+#ifndef __AFL_UTILS_H__
+#define __AFL_UTILS_H__
 
 #ifdef __ANDROID__
   #include "android-ashmem.h"
@@ -44,25 +47,35 @@
 #include <sys/types.h>
 #include <fcntl.h>
 
+u8 *__afl_area_ptr;
+u8 *__afl_fm_ptr;
+
 #ifdef __ANDROID__
-u32 afl_map_size = MAP_SIZE;
+u32 __afl_map_size = MAP_SIZE;
 #else
-__thread u32 afl_map_size = MAP_SIZE;
+__thread u32 __afl_map_size = MAP_SIZE;
 #endif
+
+/* Error reporting to forkserver controller */
+
+void send_forkserver_error(int error) {
+  u32 status;
+  if (!error || error > 0xffff) return;
+  status = (FS_OPT_ERROR | FS_OPT_SET_ERROR(error));
+  if (write(FORKSRV_FD + 1, (char *)&status, 4) != 4) return;
+}
 
 /* SHM setup. */
 
-u8 *get_fm_ptr(void) {
+static void __afl_fm_shm(void) {
   char *id_str = getenv(FM_SHM_ENV_VAR);
   u32   shm_id = atoi(id_str);
-  return shmat(shm_id, 0, 0);
+  __afl_fm_ptr = shmat(shm_id, 0, 0);
 }
 
-u8 *get_afl_area_ptr(void) {
+static void __afl_map_shm(void) {
   char *id_str = getenv(SHM_ENV_VAR);
   char *ptr;
-
-  u8 *afl_area_ptr = NULL;
 
   /* NOTE TODO BUG FIXME: if you want to supply a variable sized map then
      uncomment the following: */
@@ -71,31 +84,34 @@ u8 *get_afl_area_ptr(void) {
   if ((ptr = getenv("AFL_MAP_SIZE")) != NULL) {
 
     u32 val = atoi(ptr);
-    if (val > 0) afl_map_size = val;
+    if (val > 0) __afl_map_size = val;
 
   }
 
   */
 
-  if (afl_map_size > MAP_SIZE) {
-    if (afl_map_size > FS_OPT_MAX_MAPSIZE) {
+  if (__afl_map_size > MAP_SIZE) {
+    if (__afl_map_size > FS_OPT_MAX_MAPSIZE) {
       fprintf(stderr,
               "Error: AFL++ tools *require* to set AFL_MAP_SIZE to %u to "
               "be able to run this instrumented program!\n",
-              afl_map_size);
-      if (id_str) { return NULL; }
+              __afl_map_size);
+      if (id_str) {
+        send_forkserver_error(FS_ERROR_MAP_SIZE);
+        exit(-1);
+      }
 
     } else {
       fprintf(stderr,
               "Warning: AFL++ tools will need to set AFL_MAP_SIZE to %u to "
               "be able to run this instrumented program!\n",
-              afl_map_size);
+              __afl_map_size);
     }
   }
 
   if (id_str) {
 #ifdef USEMMAP
-    const char    *shm_file_path = id_str;
+    const char *   shm_file_path = id_str;
     int            shm_fd = -1;
     unsigned char *shm_base = NULL;
 
@@ -103,29 +119,77 @@ u8 *get_afl_area_ptr(void) {
     shm_fd = shm_open(shm_file_path, O_RDWR, 0600);
     if (shm_fd == -1) {
       fprintf(stderr, "shm_open() failed\n");
-      return NULL;
+      send_forkserver_error(FS_ERROR_SHM_OPEN);
+      exit(1);
     }
 
     /* map the shared memory segment to the address space of the process */
     shm_base =
-        mmap(0, afl_map_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        mmap(0, __afl_map_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
     if (shm_base == MAP_FAILED) {
       close(shm_fd);
       shm_fd = -1;
 
       fprintf(stderr, "mmap() failed\n");
-      return NULL;
+      send_forkserver_error(FS_ERROR_MMAP);
+      exit(2);
     }
 
-    afl_area_ptr = shm_base;
+    __afl_area_ptr = shm_base;
 #else
     u32 shm_id = atoi(id_str);
-    afl_area_ptr = shmat(shm_id, 0, 0);
-#endif
-  }
 
-  return afl_area_ptr;
+    __afl_area_ptr = shmat(shm_id, 0, 0);
+
+#endif
+
+    if (__afl_area_ptr == (void *)-1) {
+      send_forkserver_error(FS_ERROR_SHMAT);
+      exit(1);
+    }
+
+    /* Write something into the bitmap so that the parent doesn't give up */
+
+    __afl_area_ptr[0] = 1;
+  }
+}
+
+/* Fork server logic. */
+
+static void __afl_start_forkserver(void) {
+  u8  tmp[4] = {0, 0, 0, 0};
+  u32 status = 0;
+
+  if (__afl_map_size <= FS_OPT_MAX_MAPSIZE)
+    status |= (FS_OPT_SET_MAPSIZE(__afl_map_size) | FS_OPT_MAPSIZE);
+  if (status) status |= (FS_OPT_ENABLED);
+  memcpy(tmp, &status, 4);
+
+  /* Phone home and tell the parent that we're OK. */
+
+  if (write(FORKSRV_FD + 1, tmp, 4) != 4) return;
+}
+
+static u32 __afl_next_testcase(u8 *buf, u32 max_len) {
+  s32 status, res = 0xffffff;
+
+  /* Wait for parent by reading from the pipe. Abort if read fails. */
+  if (read(FORKSRV_FD, &status, 4) != 4) return 0;
+
+  /* we have a testcase - read it */
+  status = read(0, buf, max_len);
+
+  /* report that we are starting the target */
+  if (write(FORKSRV_FD + 1, &res, 4) != 4) return 0;
+
+  return status;
+}
+
+static void __afl_end_testcase(void) {
+  int status = 0xffffff;
+
+  if (write(FORKSRV_FD + 1, &status, 4) != 4) exit(1);
 }
 
 void check_sat(u8 *T, u32 npreds, u8 *afl_area_ptr, u8 *fm_map) {
@@ -151,3 +215,5 @@ void check_sat(u8 *T, u32 npreds, u8 *afl_area_ptr, u8 *fm_map) {
 
   if (fuzzmax_counter == npreds) assert(0);
 }
+
+#endif
